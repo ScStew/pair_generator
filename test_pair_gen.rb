05fea91e528d5c879bece3ff8e7ed356fd5355c3
ScStew <scottmstewart2@gmail.com>
2017-07-17 15:25:56 -0400

    added test file

---
+++ test_pair_gen.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require_relative "pair_gen.rb"
+class Pair_generator_test < Minitest::Test 
+
+	def test_1_equals_1
+		assert_equal(1,1)
+	end
+
+	def test_3_pair
+			string = ["abe","wash","george","jack","jill","hill"]
+		assert_equal(3,pair_gen(string))
+
+end
