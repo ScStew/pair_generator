1ff98928a7f9624fe5c8155abe2e64040fe0f036
ScStew <scottmstewart2@gmail.com>
2017-07-19 13:49:40 -0400

    added function that doesn't use .slice

--- pair_gen.rb
+++ pair_gen.rb
@@ -1,27 +1,54 @@
-def not_enough(string)
-	if string.length <= 3
+# def not_enough(string)
+# 	if string.length <= 3
+# 		p "not enough names"
+# 	else
+# 		random_pair(string)
+# 	end
+# end
+
+
+# def random_pair(name_arr)
+
+# 	array = name_arr.shuffle.each_slice(2).to_a
+# 	if name_arr.length % 2 == 0
+#   		array.length
+#   	else
+
+#   	array[array.length - 2]  <<  array[array.length - 1] 
+#   	array[array.length - 2].flatten!
+#   	array.slice!(array.length - 1)
+#   	p array
+#   	array.length
+# 	end	
+
+def random_pair_no_slice(name_arr)
+	if name_arr.length <= 3
 		p "not enough names"
 	else
-		random_pair(string)
+		name_arr.shuffle!
+
+		arr_1 = []
+		arr_2 = []
+
+		counter = 1
+
+		name_arr.each do |x|
+			if counter % 2 == 0
+				arr_1 << x
+			else
+				arr_2 << x
+			end
+			counter += 1
+		end
+
+		big_arr = arr_1.zip(arr_2)
+
+		if name_arr.length % 2 == 0
+			big_arr
+		else
+			big_arr.last  <<  name_arr.last
+			p big_arr
+		end
+		big_arr.length
 	end
-end
-
-
-
-
-
-def random_pair(name_arr)
-
-	array = name_arr.shuffle.each_slice(2).to_a
-	if name_arr.length % 2 == 0
-  		array.length
-  	else
-
-  	array[array.length - 2]  <<  array[array.length - 1] 
-  	array[array.length - 2].flatten!
-  	array.slice!(array.length - 1)
-  	p array
-  	array.length
-	end	
-
 end
--- test_pair_gen.rb
+++ test_pair_gen.rb
@@ -2,38 +2,48 @@ require "minitest/autorun"
 require_relative "pair_gen.rb"
 class Pair_generator_test < Minitest::Test 
 
-	def test_1_equals_1
-		assert_equal(1,1)
+	# def test_1_equals_1
+	# 	assert_equal(1,1)
+	# end
+
+	# def test_3_pair
+	# 		string = ["abe","wash","george","jack","jill","hill"]
+	# 	assert_equal(3, random_pair(string))
+	# end
+
+	# def test_3_pair_odd_number_of_names
+	# string = ["abe","wash","george","jack","jill","hill","ted"]
+	# 	assert_equal(3, random_pair(string))
+	# end
+
+	# def test_not_enough_names_2
+	# 	string = ["abe","ted"]
+	# 	assert_equal("not enough names",not_enough(string))
+	# end
+
+	# def test_not_enough_names_3
+	# 	string = ["abe","ted","jack"]
+	# 	assert_equal("not enough names",not_enough(string))
+	# end
+
+	# def test_10_names
+	# 	string = ["abe","wash","george","jack","jill","hill","ted","bill","mike","debbie"]
+	# 	assert_equal(5, not_enough(string))
+	# end
+
+	# def test_11_names
+	# 	string = ["abe","wash","george","jack","jill","hill","ted","bill","mike","debbie","stacy"]
+	# 	assert_equal(5,not_enough(string))
+	# end
+
+	def test_3_pairs_no_slice
+		names = ["abe","wash","george","jack","jill","hill","ted","bill","mike","debbie","stacy"]
+		assert_equal(5,random_pair_no_slice(names))
 	end
 
-	def test_3_pair
-			string = ["abe","wash","george","jack","jill","hill"]
-		assert_equal(3, random_pair(string))
-	end
-
-	def test_3_pair_odd_number_of_names
-	string = ["abe","wash","george","jack","jill","hill","ted"]
-		assert_equal(3, random_pair(string))
-	end
-
-	def test_not_enough_names_2
-		string = ["abe","ted"]
-		assert_equal("not enough names",not_enough(string))
-	end
-
-	def test_not_enough_names_3
-		string = ["abe","ted","jack"]
-		assert_equal("not enough names",not_enough(string))
-	end
-
-	def test_10_names
-		string = ["abe","wash","george","jack","jill","hill","ted","bill","mike","debbie"]
-		assert_equal(5, not_enough(string))
-	end
-
-	def test_11_names
-		string = ["abe","wash","george","jack","jill","hill","ted","bill","mike","debbie","stacy"]
-		assert_equal(5,not_enough(string))
+	def test_3_names_no_slice
+		names = ["abe","wash","george"]
+		assert_equal("not enough names",random_pair_no_slice(names))
 	end
 
 end
