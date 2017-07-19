c48264865425d347d4ea4de34f635131ca18986b
ScStew <scottmstewart2@gmail.com>
2017-07-19 11:09:40 -0400

    renamed argument

--- pair_gen.rb
+++ pair_gen.rb
@@ -10,14 +10,14 @@ end
 
 
 
-def random_pair(string)
+def random_pair(name_arr)
 
-	array = string.shuffle.each_slice(2).to_a
-
-	if string.length % 2 == 0
+	array = name_arr.shuffle.each_slice(2).to_a
+	if name_arr.length % 2 == 0
   		array.length
   	else
-  	array[array.length - 2]  <<  array[array.length - 1]
+
+  	array[array.length - 2]  <<  array[array.length - 1] 
   	array[array.length - 2].flatten!
   	array.slice!(array.length - 1)
   	p array
